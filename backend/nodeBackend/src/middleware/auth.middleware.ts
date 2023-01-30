12e51f45f07c4f833b36b3978db7f69e8c6ed1b7
armaankatyal <armaankatyal3@gmail.com>
2023-01-30 00:11:08 -0700

    middleware for routes

--- backend/nodeBackend/src/middleware/auth.middleware.ts
+++ backend/nodeBackend/src/middleware/auth.middleware.ts
@@ -1,6 +1,7 @@
 import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import * as dotenv from 'dotenv';
+import nodeConfig from 'config';
 dotenv.config();
 
 export const SECRET_KEY: Secret = process.env.JWT_SECRET_KEY!;
@@ -9,16 +10,68 @@ export interface CustomRequest extends Request {
     token: string | JwtPayload;
 }
 
-export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization');
+export const verifyToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
-        return res.status(401).json({ auth: false, message: 'No token provided' });
+        return res
+            .status(401)
+            .json({ error: nodeConfig.get('error_codes.TOKEN_NOT_FOUND') });
     }
     try {
         const decoded = await jwt.verify(token, SECRET_KEY);
         (req as CustomRequest).token = decoded;
         next();
-    } catch (error) {
-        return res.status(400).json({ auth: false, message: 'Failed to authenticate token' });
+    } catch (error: Error | any) {
+        // if the token is expired, return a new access token and refresh token
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({
+                error: nodeConfig.get('error_codes.TOKEN_EXPIRED'),
+            });
+        } else if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({
+                error: nodeConfig.get('error_codes.INVALID_TOKEN'),
+            });
+        } else {
+            return res.status(500).json({
+                error: nodeConfig.get('error_codes.INTERNAL_ERROR'),
+            });
+        }
+    }
+};
+
+export const verifyRefreshToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const token = req.cookies.refresh_token;
+    if (!token) {
+        return res
+            .status(401)
+            .json({ error: nodeConfig.get('error_codes.TOKEN_NOT_FOUND') });
+    }
+    try {
+        const decoded = await jwt.verify(token, SECRET_KEY);
+        (req as CustomRequest).token = decoded;
+        next();
+    } catch (error: Error | any) {
+        // if the token is expired, return a new access token and refresh token
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({
+                error: nodeConfig.get('error_codes.TOKEN_EXPIRED'),
+            });
+        } else if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({
+                error: nodeConfig.get('error_codes.INVALID_TOKEN'),
+            });
+        } else {
+            return res.status(500).json({
+                error: nodeConfig.get('error_codes.INTERNAL_ERROR'),
+            });
+        }
     }
 };
