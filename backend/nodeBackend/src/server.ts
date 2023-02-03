e5851b5aba3ee42fca7ea18163ad30b7413f6cf0
armaankatyal <armaankatyal3@gmail.com>
2023-02-03 00:05:55 -0700

    updates

--- backend/nodeBackend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from './app';
-import nodeConfig from 'config';
-
-const port = nodeConfig.get('server.port') || 8080;
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-}); // start the server
