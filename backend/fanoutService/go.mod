23541063c8e6652baad68c04a0ea546286ba0a29
armaankatyal <armaankatyal3@gmail.com>
2023-03-03 16:11:21 -0700

    design changes

--- backend/fanoutService/go.mod
+++ backend/fanoutService/go.mod
@@ -3,6 +3,7 @@ module github.com/ArmaanKatyal/tweetbit/backend/fanoutService
 go 1.20
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/spf13/viper v1.15.0
 	google.golang.org/grpc v1.53.0
 	google.golang.org/protobuf v1.28.1
--- backend/fanoutService/service/tweet.go
+++ backend/fanoutService/service/tweet.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ArmaanKatyal/tweetbit/backend/fanoutService/helpers"
 	pb "github.com/ArmaanKatyal/tweetbit/backend/fanoutService/proto"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"google.golang.org/grpc"
 )
 
@@ -35,14 +36,61 @@ func (server *FanoutServer) Run() error {
 	return s.Serve(lis)
 }
 
+type TweetPlacer struct {
+	producer      *kafka.Producer
+	topic         string
+	delivery_chan chan kafka.Event
+}
+
+func NewTweetPlacer(p *kafka.Producer, t string) *TweetPlacer {
+	return &TweetPlacer{
+		producer:      p,
+		topic:         t,
+		delivery_chan: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *TweetPlacer) PlaceTweet(tweet *pb.CreateTweetRequest) error {
+	value := tweet.GetId() + "|" + tweet.GetUuid() + "|" + tweet.GetContent()
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &op.topic, Partition: kafka.PartitionAny},
+		Value:          []byte(value),
+	}, op.delivery_chan)
+	if err != nil {
+		return err
+	}
+
+	e := <-op.delivery_chan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		return m.TopicPartition.Error
+	}
+	return nil
+}
+
 func (sever *FanoutServer) CreateTweet(_ context.Context, in *pb.CreateTweetRequest) (*pb.CreateTweetResponse, error) {
 	log.Printf("Received: %v", in.String())
-	followers := make(chan []*pb.User)
-	if helpers.StringToBool(helpers.GetConfigValue("featureFlag.enableUserGraph")) {
+	// followers := make(chan []*pb.User)
+	if helpers.StringToBool(helpers.GetConfigValue("featureFlag.enableKafka")) && helpers.StringToBool(helpers.GetConfigValue("featureFlag.enableCreateTweet")) {
 		// make a concurrent call to user graph service
-		go GetFollowers(User{in.GetId(), in.GetUuid()}, followers)
-		// TODO: goRoutine to store tweet in memory timeline for each follower
-		// TODO: goRoutine to send tweet to ActiveMQ
+		// go GetFollowers(User{in.GetId(), in.GetUuid()}, followers)
+		go func() {
+			// publish the tweet to kafka to be consumed by other services
+			p, err := kafka.NewProducer(&kafka.ConfigMap{
+				"bootstrap.servers": helpers.GetConfigValue("kafka.bootstrap.servers"),
+				"client.id":         helpers.GetConfigValue("kafka.client.id"),
+				"acks":              helpers.GetConfigValue("kafka.acks"),
+			})
+			if err != nil {
+				log.Fatalf("failed to create producer: %s", err)
+			}
+			topic := "createTweet"
+			op := NewTweetPlacer(p, topic)
+			if err = op.PlaceTweet(in); err != nil {
+				log.Fatalf("failed to place tweet: %s", err)
+			}
+		}()
 	}
 	return &pb.CreateTweetResponse{}, nil
 }
