ba65a3d324efad2fc69c51a226451add733f5c61
armaankatyal <armaankatyal3@gmail.com>
2023-03-18 00:49:22 -0600

    added viper to read config

---
+++ backend/userGraphService/helpers/viper.go
@@ -0,0 +1,33 @@
+package helpers
+
+import "github.com/spf13/viper"
+
+// returns the value of the environment variable
+func GetEnvValue(key string) string {
+	viper.SetConfigName(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		panic("Invalid type assertion")
+	}
+	return value
+}
+
+// returns the value of the config variable
+func GetConfigValue(key string) string {
+	viper.AddConfigPath("./config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		return "NO_VALUE_FOUND"
+	}
+	return value
+}
