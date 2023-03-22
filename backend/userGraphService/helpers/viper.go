0812a923702b30f4e086daadae470af108f384f9
armaankatyal <armaankatyal3@gmail.com>
2023-03-21 23:34:09 -0600

    updates

--- backend/userGraphService/helpers/viper.go
+++ backend/userGraphService/helpers/viper.go
@@ -2,20 +2,6 @@ package helpers
 
 import "github.com/spf13/viper"
 
-// returns the value of the environment variable
-func GetEnvValue(key string) string {
-	viper.SetConfigName(".env")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-	value, ok := viper.Get(key).(string)
-	if !ok {
-		panic("Invalid type assertion")
-	}
-	return value
-}
-
 // returns the value of the config variable
 func GetConfigValue(key string) string {
 	viper.SetConfigName("config")
