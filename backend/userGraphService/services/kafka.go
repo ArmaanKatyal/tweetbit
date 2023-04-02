c450a71e13e1fdba1db2ffa7728b1626eb965b07
armaankatyal <armaankatyal3@gmail.com>
2023-04-02 03:21:06 -0600

    inject & extract span from ctx

--- backend/userGraphService/services/kafka.go
+++ backend/userGraphService/services/kafka.go
@@ -1,30 +1,88 @@
 package services
 
 import (
+	"context"
 	"log"
 
 	"github.com/ArmaanKatyal/tweetbit/backend/userGraphService/constants"
+	"github.com/ArmaanKatyal/tweetbit/backend/userGraphService/helpers"
 	"github.com/ArmaanKatyal/tweetbit/backend/userGraphService/utils"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+
+	"github.com/Shopify/sarama"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/Shopify/sarama/otelsarama"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
 )
 
-// HandleRequests handles the requests from the kafka topics and calls the appropriate handlers
-func HandleRequests(consumer *kafka.Consumer, rdb *utils.RedisServer) {
-	for {
-		ev := consumer.Poll(100)
-		switch e := ev.(type) {
-		case *kafka.Message:
-			switch *e.TopicPartition.Topic {
-			case constants.CreateTweetTopic:
-				go HandleCreateTweet(e, rdb)
-			case constants.FollowUserTopic:
-				go HandleFollowUser(e, rdb.GetUserClient())
-			case constants.UnfollowUserTopic:
-				go HandleUnfollowUser(e, rdb.GetUserClient())
-			}
-		case *kafka.Error:
-			// TOOD: handle the error properly and log it
-			log.Printf("Error: %v\n", e)
-		}
+var rdb *utils.RedisServer
+
+type Consumer struct{}
+
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	log.Println("Setup has been triggered")
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/master/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		handleRequests(message)
+		session.MarkMessage(message, "")
+	}
+
+	return nil
+}
+
+func StartConsumer(rdbServer *utils.RedisServer) {
+
+	rdb = rdbServer // Set global variable
+
+	consumerGroupHandler := Consumer{}
+	// Wrap instrumentation
+	propagators := propagation.TraceContext{}
+	handler := otelsarama.WrapConsumerGroupHandler(&consumerGroupHandler, otelsarama.WithPropagators(propagators))
+
+	config := sarama.NewConfig()
+	config.Version = sarama.V2_5_0_0
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	consumer, err := sarama.NewConsumerGroup([]string{helpers.GetConfigValue("kafka.bootstrap.servers")}, helpers.GetConfigValue("kafka.group.id"), config)
+	if err != nil {
+		log.Printf("Error while creating consumer: %v", err)
+	}
+
+	err = consumer.Consume(context.Background(), []string{constants.CreateTweetTopic, constants.FollowUserTopic, constants.UnfollowUserTopic}, handler)
+	if err != nil {
+		log.Printf("Error while consuming: %v", err)
+	}
+}
+
+func handleRequests(msg *sarama.ConsumerMessage) {
+	propagaters := propagation.TraceContext{}
+	ctx := propagaters.Extract(context.Background(), otelsarama.NewConsumerMessageCarrier(msg))
+	newCtx, span := otel.Tracer("searchService").Start(ctx, "handleRequests")
+	defer span.End()
+
+	propagaters.Inject(ctx, otelsarama.NewConsumerMessageCarrier(msg))
+
+	span.SetAttributes(attribute.String("test-consumer-key", "test-consumer-value"))
+
+	switch string(msg.Topic) {
+	case constants.CreateTweetTopic:
+		go HandleCreateTweet(newCtx, msg.Value, rdb)
+	case constants.FollowUserTopic:
+		go HandleFollowUser(newCtx, msg.Value, rdb.GetUserClient())
+	case constants.UnfollowUserTopic:
+		go HandleUnfollowUser(newCtx, msg.Value, rdb.GetUserClient())
 	}
 }
